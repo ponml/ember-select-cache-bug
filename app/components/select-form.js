a74b369be0499c65f822cea1ebe6c09a5365dff8
chris <ccsand@hotmail.com>
2020-10-26 14:32:30 -0700

    init

---
+++ app/components/select-form.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-console */
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+/**
+ * Container component to display the L1 (level 1) and L2 (level2) skill selectors that will lead to an RFP questionnaire.
+ * @param {Array} l1Services - Array of L1 skills
+ *
+ * @example
+ * <SelectForm
+ *   @l1Services=l1Services
+ * />
+ */
+export default class SelectForm extends Component {
+
+  @tracked
+  selectedCategory;
+
+  @tracked
+  showServiceSelect;
+
+
+  constructor() {
+    super(...arguments);
+    console.log("form ctor")
+  }
+
+  /**
+   * Given the selected L1 service Urn event target, fetches the matching L2 services
+   * which will populate the L2 service select element
+   * @param {Object} evt - DOM select event object
+   */
+  @action
+  onSelectedCategoryChange({ target: { value: selectedCategoryId } }) {
+    console.log('selectedCategoryId' + selectedCategoryId);
+
+    this.showServiceSelect = true;
+  }
+}
---
+++ app/controllers/page.js
@@ -0,0 +1,6 @@
+import Controller from '@ember/controller';
+
+
+export default class PageController extends Controller {
+  categories = [{id: 1, name: "cat"}, {id: 2, name: "dog"}, {id: 2, name: "really big crocodile"}]
+}
